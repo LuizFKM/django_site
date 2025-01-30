caedd1c8d140ca20eae75bb48afd3300d3c02555
Luiz Francisco Kukul Marinho <127835840+LuizFKM@users.noreply.github.com>
2025-01-30 10:57:20 -0300

    Revert "Django template - exercicio modulo 10"

--- django_site/django_site/settings.py
+++ django_site/django_site/settings.py
@@ -10,12 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
-import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-TEMPLATE_DIRS = [os.path.join(BASE_DIR, "templates")]
 
 
 # Quick-start development settings - unsuitable for production
@@ -57,7 +55,7 @@ ROOT_URLCONF = 'django_site.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': TEMPLATE_DIRS,
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -124,4 +122,3 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
